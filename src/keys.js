0fa31907b84b6b5712ee85dd9f9874c21147b259
OscarAlex <43146741+OscarAlex@users.noreply.github.com>
2022-03-03 20:36:23 -0600

    Keys

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,8 @@
 module.exports= {
     mongodb: {
         URI: 'mongodb://localhost:27017/node-login-passport-local'
+    },
+    session_s: {
+        secret: 'IntruderalertRedSpyInTheBase'
     }
 }
